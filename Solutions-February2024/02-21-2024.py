8438d9d01e2bbdb98ae29a29abb4149f00272ae6
Yashika Patil <101620300+yashikapatil27@users.noreply.github.com>
2024-11-09 18:30:37 -0500

    Delete Solutions-February2024 directory

--- Solutions-February2024/02-21-2024.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        shift=0
-        while left!=right:
-            left>>=1
-            right>>=1
-            shift+=1
-        
-        return left<<shift
