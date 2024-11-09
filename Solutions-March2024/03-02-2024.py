d450d50b9d870af7abbe1ac35b182709ce8659fa
Yashika Patil <101620300+yashikapatil27@users.noreply.github.com>
2024-11-09 18:32:01 -0500

    Delete Solutions-March2024 directory

--- Solutions-March2024/03-02-2024.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        res = [0]*len(nums)
-        for i in range(len(nums)):
-            res[i] = nums[i] *nums[i]
-        res.sort()
-        return res
